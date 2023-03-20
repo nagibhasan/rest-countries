cc6bd7810b5ec9b9275a6e0871582d4ff419464d
nagibhasan <nagibhasan132@gmail.com>
2023-03-20 19:23:50 +0600

    destructoring

--- js/countries.js
+++ js/countries.js
@@ -10,13 +10,32 @@ const displayCountris = countries =>{
     container.innerHTML = CountriesHTML.join(' ');
 }
 
-const getCountryHTML = country =>{
+const getCountryHTML = ({name, flags}) =>{
     return `
         <div class="country">
-            <h2>${country.name.common}</h2>
-            <img src="${country.flags.png}">
+            <h2>${name.common}</h2>
+            <img src="${flags.png}">
         </div> 
     `
 }
+// option 1
+// const getCountryHTML = country =>{
+//     const{name, flags} = country;
+//         return `
+//         <div class="country">
+//             <h2>${name.common}</h2>
+//             <img src="${flags.png}">
+//         </div> 
+//     `
+// }
+// original
+// const getCountryHTML = country =>{
+//     return `
+//         <div class="country">
+//             <h2>${country.name.common}</h2>
+//             <img src="${country.flags.png}">
+//         </div> 
+//     `
+// }
 
 loadCountries();
